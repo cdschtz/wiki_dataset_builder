2a0679b264c573e304053eb39a34c606780f98ee
Chrisplaysnow <36952951+Chrisplaysnow@users.noreply.github.com>
2020-02-11 11:49:32 +0100

    add log sync to shell script

--- copy_generated_samples.sh
+++
@@ -1,3 +0,0 @@
-# copies all generates samples to gcloud bucket
-# excludes all files that end in .jpg or .DS_Store
-gsutil -m rsync -x ".*\.jpg$|.*\.DS_Store$" -r ./data/output_after_gen/ gs://chris-bachelor-project-bucket/data/output_after_gen/
---
+++ copy_info.sh
@@ -0,0 +1,4 @@
+# copies all generates samples to gcloud bucket
+# excludes all files that end in .jpg or .DS_Store
+gsutil -m rsync -x ".*\.jpg$|.*\.DS_Store$" -r ./data/output_after_gen/ gs://chris-bachelor-project-bucket/data/output_after_gen/
+gsutil -m rsync -x ".*\.DS_Store$" -r ./logs/ gs://chris-bachelor-project-bucket/logs/
